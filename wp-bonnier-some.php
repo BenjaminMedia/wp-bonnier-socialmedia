ae1392818dcc81dc7f6c72a4687793dde7ac5f77
Magnus Flor Johansen <magnus@m-flor.dk>
2018-08-13 15:14:47 +0200

    WILL-840 - Willow Backend release

--- wp-bonnier-some.php
+++ wp-bonnier-some.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Bonnier SoMe
- * Version: 1.1.1
+ * Version: 2.0.0
  * Plugin URI: https://github.com/BenjaminMedia/wp-bonnier-some
  * Description: Plugin for integrating social media into the application.
  * Author: Bonnier
